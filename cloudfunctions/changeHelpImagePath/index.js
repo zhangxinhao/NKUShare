d3999a1f5ee80a4bb133d84d8376ef8554b8fe53
Xinhao Zhang <inkusean@gmail.com>
2019-05-28 11:18:40 +0800

    Fix bug on update in datebase. Ref 60

---
+++ cloudfunctions/changeHelpImagePath/index.js
@@ -0,0 +1,24 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+  const {fileID, itemID} = event
+
+  const database = cloud.database({
+    env: 'nkushare-kynis'
+  })
+  await database.collection('help').doc(itemID).update({
+    data: {
+      picture: fileID
+    }
+  })
+  return {
+    event,
+    fileID,
+    itemID
+  }
+}
